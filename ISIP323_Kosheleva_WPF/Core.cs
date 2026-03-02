c0ddd62d6173bac0255de17ea37fa8dee437788a
Paris64 <ll8858261@gmail.com>
2026-03-02 09:56:51 +0300

    чо

---
+++ ISIP323_Kosheleva_WPF/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Kosheleva_WPF
+{
+
+    internal class Core
+    {
+        public static OnlineShopEntities Context = new OnlineShopEntities();
+    }
+}
