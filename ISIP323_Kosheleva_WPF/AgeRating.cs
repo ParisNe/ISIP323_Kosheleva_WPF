003e560c6504aa7c6808e65a411e5dc111c6be73
Paris64 <ll8858261@gmail.com>
2026-03-05 10:05:50 +0300

    class2

---
+++ ISIP323_Kosheleva_WPF/AgeRating.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Kosheleva_WPF
+{
+    public partial class AgeRatings
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public AgeRatings()
+        {
+            this.Movies = new HashSet<Movies>();
+        }
+
+        public int AgeRatingID { get; set; }
+        public string AgeRatingCode { get; set; }
+        public int AgeRatingValue { get; set; }
+        public string Description { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Movies> Movies { get; set; }
+    }
+}
