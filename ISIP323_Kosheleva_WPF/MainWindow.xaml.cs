78c3a27de3fc36b06bc1fab487c3687c4d0b8c5a
234959 <234959@KB37-303-C08>
2026-04-02 15:00:35 +0300

    начало

--- ISIP323_Kosheleva_WPF/MainWindow.xaml.cs
+++ ISIP323_Kosheleva_WPF/MainWindow.xaml.cs
@@ -8,53 +8,13 @@ namespace ISIP323_Kosheleva_WPF
 {
     public partial class MainWindow : Window
     {
-        private int currentStep = 1;
+        
 
         public MainWindow()
         {
             InitializeComponent();
-            UpdateProgress();
         }
 
-        private void BackButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            if (MainFrame.CanGoBack)
-            {
-                MainFrame.GoBack();
-                currentStep--;
-                UpdateProgress();
-            }
-        }
-
-        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
-        {
-            BackButton.Visibility = currentStep > 1 ? Visibility.Visible : Visibility.Collapsed;
-        }
-
-        public void GoToNextStep(string pageName)
-        {
-            if (currentStep < 5)
-            {
-                currentStep++;
-                MainFrame.Navigate(new Uri($"Pages/{pageName}.xaml", UriKind.Relative));
-                UpdateProgress();
-            }
-        }
-
-        public void GoToPreviousStep()
-        {
-            if (currentStep > 1)
-            {
-                currentStep--;
-                MainFrame.Navigate(new Uri($"Pages/Step{currentStep}.xaml", UriKind.Relative));
-                UpdateProgress();
-            }
-        }
-
-        private void UpdateProgress()
-        {
-            ProgressBar.Value = (currentStep - 1) * 25;
-            ProgressText.Text = $"Шаг {currentStep} из 5";
-        }
+        
     }
 }
